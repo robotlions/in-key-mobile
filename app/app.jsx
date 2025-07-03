8e221a677fe2379bf1afd5a6b698e42c0c7d2d8d
Chad M <chadmusick@gmail.com>
2025-07-03 17:48:41 -0400

    adds banner, intervals and minor formatting

--- app/app.jsx
+++ app/app.jsx
@@ -1,12 +1,12 @@
 import { Image } from "expo-image";
 import { useState } from "react";
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Dimensions, Pressable, StyleSheet, Text, useWindowDimensions } from "react-native";
 
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
-import { intervalSteps, intToKey, intToScale, keyArray, scaleArray } from "../data/ScaleObjects";
+import { intervalSteps, intToKey, intToScale, keyArray, minorSteps, scaleArray } from "../data/ScaleObjects";
 
 
 
@@ -16,6 +16,9 @@ export default function App() {
   const [activeScale, setActiveScale] = useState(1);
   const [scaleDegree, setScaleDegree] = useState(1);
   let currentScaleArray = [];
+
+  const scaledWidth = useWindowDimensions();
+  const intervalWidth = Dimensions.get("window").width/8
 
 
   function convertKey(interval) {
@@ -105,13 +108,14 @@ export default function App() {
 
   const currentDate = new Date();
   let currentYear = currentDate.getFullYear();
+  
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#ffffff", dark: "#000000" }}
       headerImage={
         <Image
-          source={require("@/assets/images/inKeyPastel1.jpg")}
-          style={styles.reactLogo}
+          source={require("@/assets/images/inKeyBanner_600.jpg")}
+          style={styles.bannerImage}
         />
       }
     >
@@ -150,6 +154,39 @@ export default function App() {
       </ThemedView>
 
       <ThemedView style={styles.stepContainer}><ThemedText type="subtitle">Chords in {intToKey[currentKey]} {intToScale[activeScale]}</ThemedText></ThemedView>
+    <ThemedView style={{flex:1, flexDirection:"row", justifyContent:"space-between"}}>
+          <ThemedText style={[styles.intervalContainer, {width:intervalWidth}]}>1</ThemedText>
+          <ThemedText style={[styles.intervalContainer, {width:intervalWidth}]}>2</ThemedText>
+          <ThemedText style={[styles.intervalContainer, {width:intervalWidth}]}>3</ThemedText>
+          <ThemedText style={[styles.intervalContainer, {width:intervalWidth}]}>4</ThemedText>
+          <ThemedText style={[styles.intervalContainer, {width:intervalWidth}]}>5</ThemedText>
+          <ThemedText style={[styles.intervalContainer, {width:intervalWidth}]}>6</ThemedText>
+          <ThemedText style={[styles.intervalContainer, {width:intervalWidth}]}>7</ThemedText>
+
+    </ThemedView>
+    <ThemedView style={{flex:1, flexDirection:"row", justifyContent:"space-between"}}>
+    <ThemedText style={[styles.intervalContainer, {width:intervalWidth}]}>{intToKey[currentKey]} {"\n"}
+              {minorSteps[convertScaleIntervals(0)]}
+              {pushToCurrentScaleArray(0)}</ThemedText>
+               <ThemedText style={[styles.intervalContainer, {width:intervalWidth}]}>{intToKey[convertKey(applyIntervalSteps(1))]}{"\n"}
+              {minorSteps[convertScaleIntervals(1)]}
+              {pushToCurrentScaleArray(1)}</ThemedText>
+               <ThemedText style={[styles.intervalContainer, {width:intervalWidth}]}>{intToKey[convertKey(applyIntervalSteps(2))]}{"\n"}
+              {minorSteps[convertScaleIntervals(2)]}
+              {pushToCurrentScaleArray(2)}</ThemedText>
+               <ThemedText style={[styles.intervalContainer, {width:intervalWidth}]}>{intToKey[convertKey(applyIntervalSteps(3))]}{"\n"}
+              {minorSteps[convertScaleIntervals(3)]}
+              {pushToCurrentScaleArray(3)}</ThemedText>
+               <ThemedText style={[styles.intervalContainer, {width:intervalWidth}]}>{intToKey[convertKey(applyIntervalSteps(4))]}{"\n"}
+              {minorSteps[convertScaleIntervals(4)]}
+              {pushToCurrentScaleArray(4)}</ThemedText>
+               <ThemedText style={[styles.intervalContainer, {width:intervalWidth}]}>{intToKey[convertKey(applyIntervalSteps(5))]}{"\n"}
+              {minorSteps[convertScaleIntervals(5)]}
+              {pushToCurrentScaleArray(5)}</ThemedText>
+               <ThemedText style={[styles.intervalContainer, {width:intervalWidth}]}>{intToKey[convertKey(applyIntervalSteps(6))]}{"\n"}
+              {minorSteps[convertScaleIntervals(6)]}
+              {pushToCurrentScaleArray(6)}</ThemedText>
+              </ThemedView>
     </ParallaxScrollView>
   );
 }
@@ -164,13 +201,15 @@ const styles = StyleSheet.create({
     gap: 8,
     marginBottom: 8,
   },
-  reactLogo: {
-    height: 200,
-    width: 600,
+  bannerImage: {
+    height: 300,
+    width: 550,
     bottom: 0,
     left: 0,
     position: "absolute",
   },
-
+intervalContainer:{
+  textAlign:"center"
+}
  
 });
